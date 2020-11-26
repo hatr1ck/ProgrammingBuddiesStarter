c9cdc36e1a060d3a684101a8ee4385fd7b107c98
hatr1ck <38593812+hatr1ck@users.noreply.github.com>
2020-11-26 09:53:12 +0400

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
-import { useSelector } from "react-redux";
-import "./App.scss";
-import discord from "./discord.svg";
+import NavBarFirst from "./components/navigation/navbarFirst";
+import NavBarSecond from "./components/navigation/navbarSecond";
+import Routes from "./routes/routes";
 
 function App() {
-  const result: any = useSelector((state: any) => state.reducer1);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Reddit ProgrammingBudies() group MERN starter</p>
-        <p>
-          We have here:{" "}
-          {result.map((el: string) => (
-            <span key={el}> {el},</span>
-          ))}
-        </p>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          style={{ color: "#61DAFB" }}
-          href="https://discord.gg/DbY7Fb846y"
-        >
-          {" "}
-          Discord
-          <img src={discord} alt="logo" />
-        </a>
-      </header>
+      <NavBarFirst />
+      <NavBarSecond />
+      <Routes />
     </div>
   );
 }
